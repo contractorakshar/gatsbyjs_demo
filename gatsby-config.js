e672fbea17324a896f8923ba88b862e8bbe8035d
contractorakshar <contractorakshar@gmail.com>
2021-07-22 11:39:45 +0530

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,11 @@ module.exports = {
     siteUrl: 'https://www.yourdomain.tld',
     title: 'My Gatsby Site',
   },
-  plugins: ['gatsby-plugin-gatsby-cloud'],
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+  ],
   flags: {
     DEV_SSR: false,
   },
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const HomePage = () => {
   return (
@@ -7,6 +8,11 @@ const HomePage = () => {
       <Layout pageTitle="HomePage" pageHeading="Welcome All">
         <h1>Hello Akshar</h1>
         <p>How are you?</p>
+        <StaticImage
+          alt="Blue background"
+          src="../images/images.png"
+          layout="fullWidth"
+        />
       </Layout>
     </>
   );
